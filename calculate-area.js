cc0c3481a6fd7b846b9841321551ac290c30b5f3
mrinish7 <90040174+mrinish7@users.noreply.github.com>
2021-09-29 10:21:20 +0530

    2nd commit

--- calculate-area.js
+++ calculate-area.js
@@ -2,9 +2,10 @@ const inputB = document.querySelector ('.side-one');
 const inputH = document.querySelector ('.side-two');
 const calculateBtn = document.querySelector ('#calculate-btn');
 const outputMsg = document.querySelector ('#output-msg');
-
+// console.log(inputB, typeof(inputB))
 function calculateArea() {
-    const sumOfArea = (inputB * inputH) / 2;
+    const sumOfArea = (inputB.value * inputH.value) / 2;
+    console.log(sumOfArea)
     outputMsg.innerText= "The area of trangle is " + sumOfArea
 }
 
